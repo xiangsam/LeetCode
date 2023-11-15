4cf9b394f7c58e5c6c54424bfb3a9898af05e1d5
xiangsam <huang.xiang@buaa.edu.cn>
2023-11-15 23:58:16 +0800

    add to Data_Structure

---
+++ Data_Structure/218.天际线问题.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=218 lang=cpp
+ *
+ * [218] 天际线问题
+ */
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <vector>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+  vector<vector<int>> getSkyline(vector<vector<int>> &buildings) {
+    vector<vector<int>> ans;
+    priority_queue<pair<int, int>> max_heap;
+    int n = buildings.size();
+    int i = 0, cur_x, cur_h;
+    while (i < n || !max_heap.empty()) {
+      if (max_heap.empty() ||
+          i < n && buildings[i][0] <= max_heap.top().second) {
+        cur_x = buildings[i][0];
+        while (i < n && cur_x == buildings[i][0]) {
+          max_heap.emplace(buildings[i][2], buildings[i][1]);
+          ++i;
+        }
+      } else {
+        cur_x = max_heap.top().second;
+        while (!max_heap.empty() && cur_x >= max_heap.top().second) {
+          max_heap.pop();
+        }
+      }
+      cur_h = (max_heap.empty()) ? 0 : max_heap.top().first;
+      if (ans.empty() || cur_h != ans.back()[1]) {
+        ans.push_back({cur_x, cur_h});
+      }
+    }
+    return ans;
+  }
+};
+// @lc code=end
