bc59d9352d1410f00c92ba12eb4bcad5ef473f54
xiangsam <huang.xiang@buaa.edu.cn>
2023-12-01 11:20:59 +0800

    回顾线段树与树状数组

--- Data_Structure/307.区域和检索-数组可修改(warmup).cpp
+++ Data_Structure/307.区域和检索-数组可修改(warmup).cpp
@@ -8,49 +8,27 @@
 #include <vector>
 using namespace std;
 // @lc code=start
-class NumArray1 { // TLE
-  vector<int> local_nums;
-  vector<int> psum;
-
-public:
-  NumArray1(vector<int> &nums) : local_nums(nums), psum(nums.size() + 1, 0) {
-    partial_sum(nums.begin(), nums.end(), psum.begin() + 1);
-  }
-
-  void update(int index, int val) {
-    int difference = val - local_nums[index];
-    local_nums[index] = val;
-    for (int i = index; i < local_nums.size(); ++i) {
-      psum[i + 1] += difference;
-    }
-  }
-
-  int sumRange(int left, int right) { return psum[right + 1] - psum[left]; }
-};
-
-class NumArray2 { // 树状数组
+class NumArray1 { // 树状数组
   vector<int> local_nums;
   vector<int> tree;
-
-public:
   int lowbit(int x) { return x & (-x); }
   void add(int index, int val) {
-    while (index < tree.size()) {
+    while (index <= local_nums.size()) { // tree.size() = local_nums.size()+1
       tree[index] += val;
-      index += lowbit(index); // 包含关系的父节点
+      index += lowbit(index);
     }
   }
   int prefixSum(int index) {
     int sum = 0;
     while (index > 0) {
       sum += tree[index];
-      index -=
-          lowbit(index); // 当前tree[index]管辖为[index - lowbit(index)+1,index]
+      index -= lowbit(index);
     }
     return sum;
   }
 
-  NumArray2(vector<int> &nums) : local_nums(nums), tree(nums.size() + 1, 0) {
+public:
+  NumArray1(vector<int> &nums) : local_nums(nums), tree(nums.size() + 1, 0) {
     for (int i = 0; i < nums.size(); ++i) {
       add(i + 1, nums[i]);
     }
@@ -65,56 +43,57 @@ public:
     return prefixSum(right + 1) - prefixSum(left);
   }
 };
-
 class NumArray { // 线段树
-  vector<int> tree;
   int n;
-  void build(int node, int s, int e, vector<int> &nums) {
+  vector<int> tree;
+  void build(int s, int e, int node, vector<int> &nums) {
     if (s == e) {
       tree[node] = nums[s];
       return;
     }
     int m = s + ((e - s) >> 1);
-    build(node * 2 + 1, s, m, nums);
-    build(node * 2 + 2, m + 1, e, nums);
+    build(s, m, node * 2 + 1, nums);
+    build(m + 1, e, node * 2 + 2, nums);
     tree[node] = tree[node * 2 + 1] + tree[node * 2 + 2];
   }
-  void update_tree(int index, int val, int node, int s, int e) {
+  void update_tree(int index, int val, int s, int e, int node) {
     if (s == e) {
       tree[node] = val;
       return;
     }
     int m = s + ((e - s) >> 1);
     if (index <= m) {
-      update_tree(index, val, node * 2 + 1, s, m);
+      update_tree(index, val, s, m, node * 2 + 1);
     } else {
-      update_tree(index, val, node * 2 + 2, m + 1, e);
+      update_tree(index, val, m + 1, e, node * 2 + 2);
     }
     tree[node] = tree[node * 2 + 1] + tree[node * 2 + 2];
   }
-  int getSum(int l, int r, int node, int s, int e) {
-    int sum = 0;
+  int prefixSum(int l, int r, int s, int e, int node) {
     if (l <= s && r >= e) {
       return tree[node];
     }
+    int sum = 0;
     int m = s + ((e - s) >> 1);
     if (l <= m) {
-      sum += getSum(l, r, node * 2 + 1, s, m);
+      sum += prefixSum(l, r, s, m, node * 2 + 1);
     }
     if (r > m) {
-      sum += getSum(l, r, node * 2 + 2, m + 1, e);
+      sum += prefixSum(l, r, m + 1, e, node * 2 + 2);
     }
     return sum;
   }
 
 public:
-  NumArray(vector<int> &nums) : tree(nums.size() * 4, 0), n(nums.size()) {
-    build(0, 0, n - 1, nums);
+  NumArray(vector<int> &nums) : n(nums.size()), tree(4 * n, 0) {
+    build(0, n - 1, 0, nums);
   }
 
-  void update(int index, int val) { update_tree(index, val, 0, 0, n - 1); }
+  void update(int index, int val) { update_tree(index, val, 0, n - 1, 0); }
 
-  int sumRange(int left, int right) { return getSum(left, right, 0, 0, n - 1); }
+  int sumRange(int left, int right) {
+    return prefixSum(left, right, 0, n - 1, 0);
+  }
 };
 /**
  * Your NumArray object will be instantiated and called as such:
