e9682026e15224106f5f2ce22263b6db5a774e2d
xiangsam <huang.xiang@buaa.edu.cn>
2023-12-10 10:58:16 +0800

    add to Linked List

--- Linked_List/21.合并两个有序链表.cpp
+++ Linked_List/21.合并两个有序链表.cpp
@@ -17,7 +17,7 @@
  */
 class Solution {
 public:
-  ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+  ListNode *mergeTwoLists1(ListNode *list1, ListNode *list2) {
     ListNode *p;
     p = new ListNode(0, nullptr);
     ListNode *dump = p;
@@ -42,6 +42,27 @@ public:
       list2 = list2->next;
     }
     return dump->next;
+  }
+  ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+    if (list1 == nullptr) {
+      return list2;
+    } else if (list2 == nullptr) {
+      return list1;
+    }
+    auto cmp = [](ListNode *a, ListNode *b) { return a->val > b->val; };
+    priority_queue<ListNode *, vector<ListNode *>, decltype(cmp)> q(cmp);
+    q.push(list1);
+    q.push(list2);
+    ListNode *ans = new ListNode(0, nullptr), *cur = ans;
+    while (!q.empty()) {
+      cur->next = q.top();
+      q.pop();
+      cur = cur->next;
+      if (cur->next) {
+        q.push(cur->next);
+      }
+    }
+    return ans->next;
   }
 };
 // @lc code=end
