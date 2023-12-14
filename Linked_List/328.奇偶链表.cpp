a1838c81074bae87749eaa602a0e174c1ac699e4
xiangsam <huang.xiang@buaa.edu.cn>
2023-12-14 08:48:25 +0800

    add to Linked List

---
+++ Linked_List/328.奇偶链表.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=cpp
+ *
+ * [328] 奇偶链表
+ */
+#include <iostream>
+using namespace std;
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *oddEvenList(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) {
+      return head;
+    }
+    ListNode *odd = head, *even = head->next;
+    ListNode *dummy = head->next;
+    while (true) {
+      if (even->next != nullptr) {
+        odd->next = even->next;
+        odd = odd->next;
+      } else {
+        odd->next = nullptr;
+        break;
+      }
+      if (odd->next != nullptr) {
+        even->next = odd->next;
+        even = even->next;
+      } else {
+        even->next = nullptr;
+        break;
+      }
+    }
+    odd->next = dummy;
+    return head;
+  }
+};
+// @lc code=end
