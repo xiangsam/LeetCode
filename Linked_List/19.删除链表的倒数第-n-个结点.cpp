a7e534962292f6ee4082d55d9f779df97ed6874b
xiangsam <huang.xiang@buaa.edu.cn>
2023-12-15 10:32:49 +0800

    add to Linked List

---
+++ Linked_List/19.删除链表的倒数第-n-个结点.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=cpp
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+#include <cstddef>
+#include <iostream>
+using namespace std;
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+
+public:
+  ListNode *findNthFromEnd(ListNode *head, int &count) {
+    if (head == nullptr) {
+      return nullptr;
+    }
+    ListNode *node = findNthFromEnd(head->next, count);
+    --count;
+    if (count == 0) {
+      return head;
+    }
+    return node;
+  }
+  ListNode *removeNthFromEnd(ListNode *head, int n) {
+    ListNode *dummy = new ListNode(0);
+    dummy->next = head;
+    int count = n + 1;
+    ListNode *p = findNthFromEnd(dummy, count);
+    ListNode *target = p->next;
+    p->next = target->next;
+    delete target;
+    return dummy->next;
+  }
+};
+// @lc code=end
