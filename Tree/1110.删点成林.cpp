5cc39f883303c64f9f2a1ac765863910aa84fc36
xiangsam <huang.xiang@buaa.edu.cn>
2023-12-19 10:50:35 +0800

    add to Tree

--- Tree/1110.删点成林.cpp
+++ Tree/1110.删点成林.cpp
@@ -29,12 +29,13 @@ struct TreeNode {
  * right(right) {}
  * };
  */
-class Solution {
+class Solution1 {
   unordered_set<int> targets;
   vector<TreeNode *> ans;
 
 public:
   void dfs(TreeNode *root) {
+    // 不删除节点，需要额外判断子节点是否在删除状态中
     if (root == nullptr) {
       return;
     }
@@ -67,4 +68,41 @@ public:
     return ans;
   }
 };
+class Solution {
+  unordered_set<int> targets;
+  vector<TreeNode *> ans;
+
+public:
+  TreeNode *dfs(TreeNode *root) {
+    // 删除节点，判断子节点是否为null即可
+    if (root == nullptr) {
+      return root;
+    }
+    root->left = dfs(root->left);
+    root->right = dfs(root->right);
+    if (targets.contains(root->val)) {
+      if (root->left) {
+        ans.emplace_back(root->left);
+      }
+      if (root->right) {
+        ans.emplace_back(root->right);
+      }
+      // delete root;
+      root = nullptr;
+    }
+    return root;
+  }
+  vector<TreeNode *> delNodes(TreeNode *root, vector<int> &to_delete) {
+    // 先处理完子节点，再处理当前节点，避免断开链接导致遍历问题
+    for (const auto &e : to_delete) {
+      targets.insert(e);
+    }
+    root = dfs(root);
+    if (root) {
+      ans.emplace_back(root);
+    }
+    return ans;
+  }
+};
+
 // @lc code=end
