e56c65ddada8760a6b4508a03552739f1ef1be5a
xiangsam <huang.xiang@buaa.edu.cn>
2023-12-21 18:29:43 +0800

    add to Tree

--- Tree/144.二叉树的前序遍历.cpp
+++ Tree/144.二叉树的前序遍历.cpp
@@ -4,6 +4,7 @@
  * [144] 二叉树的前序遍历
  */
 #include <iostream>
+#include <stack>
 #include <vector>
 using namespace std;
 struct TreeNode {
@@ -40,8 +41,27 @@ public:
     preorderDFS(root->left);
     preorderDFS(root->right);
   }
-  vector<int> preorderTraversal(TreeNode *root) {
+  vector<int> preorderTraversal1(TreeNode *root) {
     preorderDFS(root);
+    return ans;
+  }
+  vector<int> preorderTraversal(TreeNode *root) {
+    if (root == nullptr) {
+      return ans;
+    }
+    stack<TreeNode *> stk;
+    stk.emplace(root);
+    while (!stk.empty()) {
+      TreeNode *top = stk.top();
+      stk.pop();
+      ans.emplace_back(top->val);
+      if (top->right) {
+        stk.emplace(top->right);
+      }
+      if (top->left) {
+        stk.emplace(top->left);
+      }
+    }
     return ans;
   }
 };
