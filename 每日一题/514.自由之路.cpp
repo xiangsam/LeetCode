1f7bce39bcededaab9e610c78bcca9069f3b8947
xiangsam <huang.xiang@buaa.edu.cn>
2024-01-29 18:48:52 +0800

    add to 每日一题

--- 每日一题/514.自由之路.cpp
+++ 每日一题/514.自由之路.cpp
@@ -10,7 +10,7 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    int findRotateSteps(string ring, string key) {
+    int findRotateSteps1(string ring, string key) {
         // dp[i][j]: 拼出key的第i个字符，ring的第j个字符指向12:00的步数
         // dp[i][j]只有在key[i]与ring[j]相等时才有意义，使用pos[i]表示合法的key[i]合法的j
         // 状态转移：dp[i][j](j\in pos[i]) = min{dp[i-1][k](k\in pos[i-1]) + min{abs{j-k}, n-abs{j-k}}}
@@ -32,6 +32,29 @@ public:
         }
         int ans = INT_MAX;
         for(const auto idx : pos[key[m-1] - 'a']){
+            ans = min(ans, dp[m-1][idx]);
+        }
+        return ans + m;
+    }
+    int findRotateSteps(string ring, string key) {
+        int n = ring.length(), m = key.length();
+        vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
+        vector<vector<int>> pos(26);
+        for(auto i = 0; i < n; ++i){
+            pos[ring[i] - 'a'].emplace_back(i);
+        }
+        for(const auto & idx : pos[key[0] - 'a']){
+            dp[0][idx] = min(idx, n - idx);
+        }
+        for(int i = 1; i < m; ++i){
+            for(const auto & idx1 : pos[key[i] - 'a']){
+                for(const auto & idx2 : pos[key[i-1] - 'a']){
+                    dp[i][idx1] = min(dp[i][idx1], dp[i-1][idx2] + min(abs(idx1 - idx2), n - abs(idx1 - idx2)));
+                }
+            }
+        }
+        int ans = INT_MAX;
+        for(const auto & idx : pos[key[m-1] - 'a']){
             ans = min(ans, dp[m-1][idx]);
         }
         return ans + m;
