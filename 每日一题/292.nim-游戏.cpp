cd6c7f54b5e46ea0ca3b53176141e2a11b1332b8
xiangsam <huang.xiang@buaa.edu.cn>
2024-02-04 10:37:51 +0800

    add to 每日一题

---
+++ 每日一题/292.nim-游戏.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=292 lang=cpp
+ *
+ * [292] Nim 游戏
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    bool canWinNim1(int n) {
+        vector<int> canWin(n+1, 0);
+        for(int i = 0; i <=3 && i <= n; ++i){
+            canWin[i] = 1;
+        }
+        for(int i = 4; i <= n; ++i){
+            if(canWin[i-1] == -1 || canWin[i-2] == -1 || canWin[i-3] == -1)
+                canWin[i] = 1;
+            else
+                canWin[i] = -1;
+        }
+        return canWin[n] == 1;
+    }   
+    bool canWinNim(int n) {
+        return n % 4 > 0;
+    } 
+};
+// @lc code=end
+
